9e63df6077e19a644ae1356e2e51120990beaf67
Alexandre Moreira Correa <alexandremoreira_.m@hotmail.com>
2025-08-24 20:56:23 +0100

    responsividade1

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: './',
-});
+  base: './',
+  server: {
+    host: true, 
+    port: 5173,
+  }
+})
